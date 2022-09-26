ce19693d586a2cfc099b770723b87b694946d37a
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-09-26 22:31:57 +0700

    Hilangin Laporan dan pengaturan users

--- application/views/Tamplate/sidebar.php
+++ application/views/Tamplate/sidebar.php
@@ -27,39 +27,9 @@
                         </ul>
                     </li>
                     <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="<?= site_url()?>Transaksi" aria-expanded="false"><i data-feather="dollar-sign" class="feather-icon"></i><span class="hide-menu">Transaksi</span></a></li>
-                    <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false"><i data-feather="book-open" class="feather-icon"></i><span class="hide-menu">Laporan</span></a>
-                        <ul aria-expanded="false" class="collapse  first-level base-level-line">
-                            <li class="sidebar-item"><a href="<?= site_url()?>Laporan/barang" class="sidebar-link"><span
-                                        class="hide-menu"> Laporan Barang
-                                    </span></a>
-                            </li>
-                            <li class="sidebar-item"><a href="<?= site_url()?>Laporan/barang_masuk" class="sidebar-link"><span
-                                        class="hide-menu"> Laporan Barang Masuk
-                                    </span></a>
-                            </li>
-                            <li class="sidebar-item"><a href="<?= site_url()?>Laporan/perbulan" class="sidebar-link"><span
-                                        class="hide-menu"> Laporan Data Perbulan
-                                    </span></a>
-                            </li>
-                        </ul>
-                    </li>
+                    
                     <li class="list-divider"></li>
-                    <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false"><i data-feather="user" class="feather-icon"></i><span class="hide-menu">Pengaturan User</span></a>
-                        <ul aria-expanded="false" class="collapse  first-level base-level-line">
-                            <li class="sidebar-item"><a href="<?= site_url()?>Pengaturan_user/permintaan_reg" class="sidebar-link"><span
-                                        class="hide-menu"> Permintaan Registrasi
-                                    </span></a>
-                            </li>
-                            <li class="sidebar-item"><a href="<?= site_url()?>Pengaturan_user/manajemen_user" class="sidebar-link"><span
-                                        class="hide-menu"> Manajemen Pengguna
-                                    </span></a>
-                            </li>
-                            <li class="sidebar-item"><a href="<?= site_url()?>Pengaturan_user/ganti_password" class="sidebar-link"><span
-                                        class="hide-menu"> Ganti Kata Sandi
-                                    </span></a>
-                            </li>
-                        </ul>
-                    </li>
+                    
                     <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="<?= site_url()?>Auth/logout"
                             aria-expanded="false"><i data-feather="log-out" class="feather-icon"></i><span
                                 class="hide-menu">Logout</span></a></li>
