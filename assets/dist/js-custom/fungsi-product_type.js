f92ecaf519bcec963a35f7978dfee612c3b5247a
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-10-03 20:00:56 +0700

    Menghilangkan fungsi tambah data dan hapus data

--- assets/dist/js-custom/fungsi-product_type.js
+++ assets/dist/js-custom/fungsi-product_type.js
@@ -23,164 +23,165 @@ $(document).ready(function(){
 
     });
 
-    function reload_table()
-    {
-        table.ajax.reload(null,false); //reload datatable ajax 
-    }
+    // function reload_table()
+    // {
+    //     table.ajax.reload(null,false); //reload datatable ajax 
+    // }
 
     // validasi form
-    $("#nama").keyup(function(){
-        var validasiHuruf = /^[a-zA-Z0-9 ]+$/;
-        var nama = $('#nama').val();
-        if(nama == 0)
-        {
-            $('#nama').removeClass('is-valid');
-            $('#nama').addClass('is-invalid');
+    // $("#nama").keyup(function(){
+    //     var validasiHuruf = /^[a-zA-Z0-9 ]+$/;
+    //     var nama = $('#nama').val();
+    //     if(nama == 0)
+    //     {
+    //         $('#nama').removeClass('is-valid');
+    //         $('#nama').addClass('is-invalid');
 
-            $('#hintnama').removeClass('text-muted');
-            $('#hintnama').removeClass('text-success');
-            $('#hintnama').addClass('text-danger');
+    //         $('#hintnama').removeClass('text-muted');
+    //         $('#hintnama').removeClass('text-success');
+    //         $('#hintnama').addClass('text-danger');
 
-            $('#hintnama').text("nama kategori tidak boleh kosong");
-        } 
+    //         $('#hintnama').text("nama tipe produk tidak boleh kosong");
+    //     } 
                
-        else
-        {
-            if(nama.match(validasiHuruf))
-            {
-                $.ajax({
-                    type: "POST",
-                    url: "Product_type/cek_nama",
-                    data: $(this).serialize(),
-                    success: function (data){
-                        // console.log(data);
-                        if(data=="nama ada")
-                        {
-                            $('#nama').removeClass('is-valid');
-                            $('#nama').addClass('is-invalid');
+    //     else
+    //     {
+    //         if(nama.match(validasiHuruf))
+    //         {
+    //             $.ajax({
+    //                 type: "POST",
+    //                 url: "Product_type/cek_nama",
+    //                 data: $(this).serialize(),
+    //                 success: function (data){
+    //                     // console.log(data);
+    //                     if(data=="nama ada")
+    //                     {
+    //                         $('#nama').removeClass('is-valid');
+    //                         $('#nama').addClass('is-invalid');
 
-                            $('#hintnama').removeClass('text-muted');
-                            $('#hintnama').removeClass('text-success');
-                            $('#hintnama').addClass('text-danger');
+    //                         $('#hintnama').removeClass('text-muted');
+    //                         $('#hintnama').removeClass('text-success');
+    //                         $('#hintnama').addClass('text-danger');
 
-                            $('#hintnama').text("nama '"+nama+"' sudah digunakan"); 
-                        } 
-                        else 
-                        {
-                            $('#nama').removeClass('is-invalid');
-                            $('#nama').removeClass('is-valid');
+    //                         $('#hintnama').text("nama '"+nama+"' sudah digunakan"); 
+    //                     } 
+    //                     else 
+    //                     {
+    //                         $('#nama').removeClass('is-invalid');
+    //                         $('#nama').removeClass('is-valid');
     
-                            $('#hintnama').removeClass('text-success');
-                            $('#hintnama').removeClass('text-danger');
-                            $('#hintnama').addClass('text-muted');
+    //                         $('#hintnama').removeClass('text-success');
+    //                         $('#hintnama').removeClass('text-danger');
+    //                         $('#hintnama').addClass('text-muted');
     
-                            $('#hintnama').text("*max 20 karakter.");  
-                        }
-                    }
-                 });
-            } 
-            else
-            {
-                $('#nama').removeClass('is-valid');
-                $('#nama').addClass('is-invalid');
+    //                         $('#hintnama').text("*max 20 karakter.");  
+    //                     }
+    //                 }
+    //              });
+    //         } 
+    //         else
+    //         {
+    //             $('#nama').removeClass('is-valid');
+    //             $('#nama').addClass('is-invalid');
 
-                $('#hintnama').removeClass('text-muted');
-                $('#hintnama').removeClass('text-success');
-                $('#hintnama').addClass('text-danger');
+    //             $('#hintnama').removeClass('text-muted');
+    //             $('#hintnama').removeClass('text-success');
+    //             $('#hintnama').addClass('text-danger');
         
-                $('#hintnama').text("*tidak boleh mengandung karakter simbol."); 
-            }           
-        }
-    });
+    //             $('#hintnama').text("*tidak boleh mengandung karakter simbol."); 
+    //         }           
+    //     }
+    // });
+
     //Save data
-    $('#btn_save').on('click',function(){
-        var nama = $('#nama').val();
-        var validasi = /^[a-zA-Z0-9 ]*$/;
-        var namahasClass = $("#nama").hasClass("is-invalid");
-        if(nama == 0)
-        {
-            $('#nama').addClass('is-invalid');
+    // $('#btn_save').on('click',function(){
+    //     var nama = $('#nama').val();
+    //     var validasi = /^[a-zA-Z0-9 ]*$/;
+    //     var namahasClass = $("#nama").hasClass("is-invalid");
+    //     if(nama == 0)
+    //     {
+    //         $('#nama').addClass('is-invalid');
 
-            $('#hintnama').removeClass('text-muted');
-            $('#hintnama').addClass('text-danger');
+    //         $('#hintnama').removeClass('text-muted');
+    //         $('#hintnama').addClass('text-danger');
 
-            $('#hintnama').text("nama kategori tidak boleh kosong");
-        }    
-        else if(namahasClass)
-        {
-            e.preventDefault();
-        }
-        else
-        {
-            if(nama.match(validasi))
-            {
-                $.ajax({
-                    type : "POST",
-                    url  : "Product_type/add",
-                    dataType : "JSON",
-                    data : {nama:nama},
-                    success: function(data){
-                        Swal.fire(
-                            'Simpan!',
-                            'Data berhasil disimpan.',
-                            'success'
-                          )
-                        $('[name="nama"]').val("");
-                        $('#tambah-data').modal('hide');
-                        reload_table()
+    //         $('#hintnama').text("nama kategori tidak boleh kosong");
+    //     }    
+    //     else if(namahasClass)
+    //     {
+    //         e.preventDefault();
+    //     }
+    //     else
+    //     {
+    //         if(nama.match(validasi))
+    //         {
+    //             $.ajax({
+    //                 type : "POST",
+    //                 url  : "Product_type/add",
+    //                 dataType : "JSON",
+    //                 data : {nama:nama},
+    //                 success: function(data){
+    //                     Swal.fire(
+    //                         'Simpan!',
+    //                         'Data berhasil disimpan.',
+    //                         'success'
+    //                       )
+    //                     $('[name="nama"]').val("");
+    //                     $('#tambah-data').modal('hide');
+    //                     reload_table()
                         
-                    }
-                });
-                return false;
-            } 
-            else
-            {
-                $('#nama').removeClass('is-valid');
-                $('#nama').addClass('is-invalid');
+    //                 }
+    //             });
+    //             return false;
+    //         } 
+    //         else
+    //         {
+    //             $('#nama').removeClass('is-valid');
+    //             $('#nama').addClass('is-invalid');
 
-                $('#hintnama').removeClass('text-muted');
-                $('#hintnama').removeClass('text-success');
-                $('#hintnama').addClass('text-danger');
+    //             $('#hintnama').removeClass('text-muted');
+    //             $('#hintnama').removeClass('text-success');
+    //             $('#hintnama').addClass('text-danger');
         
-                $('#hintnama').text("*tidak boleh mengandung karakter simbol."); 
-            } 
-        }        
-    });
+    //             $('#hintnama').text("*tidak boleh mengandung karakter simbol."); 
+    //         } 
+    //     }        
+    // });
 
     //get data for delete record
-    $('#tableKategori').on('click','.delete_kategori',function(){
-        var kd_kategori = $(this).data('kd_kategori');
+    // $('#tableKategori').on('click','.delete_kategori',function(){
+    //     var kd_kategori = $(this).data('kd_kategori');
          
-        // $('#Modal_Delete').modal('show');
-        // $('[name="product_code_delete"]').val(product_code);
-        Swal.fire({
-            title: 'Apa anda yakin?',
-            text: "mengahapus data ini",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Hapus',
-            cancelButtonText: 'Batal'
-          }).then((result) => {
-            if (result.isConfirmed) {
-                $.ajax({
-                    type : "POST",
-                    url  : "Product_type/delete",
-                    dataType : "JSON",
-                    data : {kd_kategori:kd_kategori},
-                    success: function(data){
-                        Swal.fire(
-                            'Hapus!',
-                            'Data berhasil dihapus.',
-                            'success'
-                          )
-                        reload_table()
-                    }
-                });
-                return false;
+    //     // $('#Modal_Delete').modal('show');
+    //     // $('[name="product_code_delete"]').val(product_code);
+    //     Swal.fire({
+    //         title: 'Apa anda yakin?',
+    //         text: "mengahapus data ini",
+    //         icon: 'warning',
+    //         showCancelButton: true,
+    //         confirmButtonColor: '#3085d6',
+    //         cancelButtonColor: '#d33',
+    //         confirmButtonText: 'Hapus',
+    //         cancelButtonText: 'Batal'
+    //       }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             $.ajax({
+    //                 type : "POST",
+    //                 url  : "Product_type/delete",
+    //                 dataType : "JSON",
+    //                 data : {kd_kategori:kd_kategori},
+    //                 success: function(data){
+    //                     Swal.fire(
+    //                         'Hapus!',
+    //                         'Data berhasil dihapus.',
+    //                         'success'
+    //                       )
+    //                     reload_table()
+    //                 }
+    //             });
+    //             return false;
             
-            }
-          })
-    });
+    //         }
+    //       })
+    // });
 });
