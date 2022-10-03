c73bfde46f78276a915b07f8695d906e392e5460
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-10-03 13:33:33 +0700

    Ubah login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -27,50 +27,50 @@ class Auth extends CI_Controller {
 
 	public function cek_username()
 	{         
-		$username = html_escape($this->input->post('username'));
-		$cekUser = $this->m_users->get_username($username);
+		$clogin = html_escape($this->input->post('username'));
+		$cekUser = $this->m_users->get_username($clogin);
 		if ($cekUser > 0)
 		{
 			echo "username";
 		}
 	}
 
-	public function proses_register()
-	{
-		$karakter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-        $shuffle10  = substr(str_shuffle($karakter), 0, 10);
-        $shuffle5  = substr(str_shuffle($karakter), 0, 5);
-		$date = date('dmYhis');
-		$waktu_reg = date('Y-m-d');
-		$kd_user = $shuffle10.$date.$shuffle5;
+	// public function proses_register()
+	// {
+	// 	$karakter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    //     $shuffle10  = substr(str_shuffle($karakter), 0, 10);
+    //     $shuffle5  = substr(str_shuffle($karakter), 0, 5);
+	// 	$date = date('dmYhis');
+	// 	$waktu_reg = date('Y-m-d');
+	// 	$kd_user = $shuffle10.$date.$shuffle5;
 
-		$nama = html_escape($this->input->post('nama'));
-		$username = html_escape($this->input->post('username'));
-		$akses = html_escape($this->input->post('akses'));
-		$password = html_escape($this->input->post('pass1'));
-		$passhash = password_hash($password, PASSWORD_DEFAULT);
-		$foto_default = "Default.png";
+	// 	$nama = html_escape($this->input->post('nama'));
+	// 	$clogin = html_escape($this->input->post('username'));
+	// 	$akses = html_escape($this->input->post('akses'));
+	// 	$password = html_escape($this->input->post('pass1'));
+	// 	$passhash = password_hash($password, PASSWORD_DEFAULT);
+	// 	$foto_default = "Default.png";
 
-		$data = array(
-			'kd_user' => $kd_user,
-			'username' => $username,
-			'nama_user' => $nama,
-			'password' => $passhash,
-			'kd_role' => $akses,
-			'waktu_reg' => $waktu_reg,
-			'foto' => $foto_default
-			);			
-			$this->m_users->save($data);
-		$this->session->set_flashdata('flash', 'berhasil mendaftar');
-		redirect('Auth');
-	}
+	// 	$data = array(
+	// 		'kd_user' => $kd_user,
+	// 		'username' => $clogin,
+	// 		'nama_user' => $nama,
+	// 		'password' => $passhash,
+	// 		'kd_role' => $akses,
+	// 		'waktu_reg' => $waktu_reg,
+	// 		'foto' => $foto_default
+	// 		);			
+	// 		$this->m_users->save($data);
+	// 	$this->session->set_flashdata('flash', 'berhasil mendaftar');
+	// 	redirect('Auth');
+	// }
 
 	public function proses_login()
 	{
-		$username = html_escape($this->input->post('username'));
+		$clogin = html_escape($this->input->post('username'));
 		$password = html_escape(md5($this->input->post('password')));
 
-		$data = $this->m_users->get_users($username);
+		$data = $this->m_users->get_users($clogin);
 		if(empty($data))
 		{
 			$this->session->set_flashdata('flash', 'username salah');
@@ -78,12 +78,7 @@ class Auth extends CI_Controller {
 		}
 		else
 		{
-			if($data->req == 0)
-			{
-				$this->session->set_flashdata('flash', 'belum terdaftar');
-				redirect('Auth');
-			}
-			else if($data->status == 0)
+			if($data->active == 0)
 			{
 				$this->session->set_flashdata('flash', 'status nonaktif');
 				redirect('Auth');
@@ -92,14 +87,12 @@ class Auth extends CI_Controller {
 			{
 				if($password == $data->password)
 				{		
-					$kd_user = $data->kd_user;	
-					$cek_role = $data->kd_role;	
+					$seq = $data->seq;	
+					// $cek_role = $data->kd_role;	
 					
-					$this->session->set_userdata('kd_user', $kd_user);
-					$this->session->set_userdata('nama', $data->nama_user);	
-					$this->session->set_userdata('kd_role', $data->kd_role);	
-					$this->session->set_userdata('username', $username);
-					$this->session->set_userdata('foto', $data->foto);		
+					$this->session->set_userdata('seq', $seq);
+					$this->session->set_userdata('cname', $data->cname);		
+					$this->session->set_userdata('clogin', $clogin);		
 					$this->session->set_userdata('is_login',TRUE);	
 					redirect('Dashboard');				
 				}
--- application/models/m_users.php
+++ application/models/m_users.php
@@ -3,117 +3,119 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class m_users extends CI_Model {
 
-    public function get_username($username)
+    public function get_clogin($clogin)
     {
-        $this->db->where('username',$username);
-        $query = $this->db->get('users');
+        $this->db->where('clogin',$clogin);
+        $query = $this->db->get('zuser');
         return $query->num_rows();
     }
 
-    public function register($user)
-    {
-        return $this->db->insert('users', $user);
-    }
+    // public function register($user)
+    // {
+    //     return $this->db->insert('zuser', $user);
+    // }
 
-    public function get_users($username)
+    public function get_users($clogin)
     {
-        $this->db->where('username',$username);
-        $result = $this->db->get('users')->row();
+        $this->db->where('clogin',$clogin);
+        $result = $this->db->get('zuser')->row();
         return $result;
     }
 
-    public function update_status($status, $username)
+    public function update_status($status, $clogin)
     {
         $this->db->set('status', $status);
-        $this->db->where('username', $username);
-        $this->db->update('users');
+        $this->db->where('clogin', $clogin);
+        $this->db->update('zuser');
     }
 
-    // Simpan ke database users
-    public function save($data)
-    {
-        $this->db->insert('users', $data);
-    }
+    // // Simpan ke database users
+    // public function save($data)
+    // {
+    //     $this->db->insert('zuser', $data);
+    // }
 
-    var $table = 'users as u'; //nama tabel dari database
-    var $column_order = array(null, 'u.kd_user', 'u.username','u.nama_user','r.nama_akses'); //field yang ada di table user
-    var $column_search = array('u.username','u.nama_user'); //field yang diizin untuk pencarian 
-    var $order = array('u.nama_user' => 'asc'); // default order 
+    // var $table = 'users as u'; //nama tabel dari database
+    // var $column_order = array(null, 'u.seq', 'u.clogin','u.nama_user','r.nama_akses'); //field yang ada di table user
+    // var $column_search = array('u.clogin','u.nama_user'); //field yang diizin untuk pencarian 
+    // var $order = array('u.nama_user' => 'asc'); // default order 
     
-    private function _get_datatables_query()
-    {
+    // private function _get_datatables_query()
+    // {
          
-        $this->db->from($this->table);
-        $this->db->join('akses as r', 'r.kd_role = u.kd_role');
-        $this->db->where('req', 1);
-        $i = 0;
+    //     $this->db->from($this->table);
+    //     $this->db->join('akses as r', 'r.kd_role = u.kd_role');
+    //     $this->db->where('req', 1);
+    //     $i = 0;
      
-        foreach ($this->column_search as $item) // looping awal
-        {
-            if($_POST['search']['value']) // jika datatable mengirimkan pencarian dengan metode POST
-            {
+    //     foreach ($this->column_search as $item) // looping awal
+    //     {
+    //         if($_POST['search']['value']) // jika datatable mengirimkan pencarian dengan metode POST
+    //         {
                  
-                if($i===0) // looping awal
-                {
-                    $this->db->group_start(); 
-                    $this->db->like($item, $_POST['search']['value']);
-                }
-                else
-                {
-                    $this->db->or_like($item, $_POST['search']['value']);
-                }
+    //             if($i===0) // looping awal
+    //             {
+    //                 $this->db->group_start(); 
+    //                 $this->db->like($item, $_POST['search']['value']);
+    //             }
+    //             else
+    //             {
+    //                 $this->db->or_like($item, $_POST['search']['value']);
+    //             }
  
-                if(count($this->column_search) - 1 == $i) 
-                    $this->db->group_end(); 
-            }
-            $i++;
-        }
+    //             if(count($this->column_search) - 1 == $i) 
+    //                 $this->db->group_end(); 
+    //         }
+    //         $i++;
+    //     }
          
-        if(isset($_POST['order'])) 
-        {
-            $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
-        } 
-        else if(isset($this->order))
-        {
-            $order = $this->order;
-            $this->db->order_by(key($order), $order[key($order)]);
-        }
-    }
+    //     if(isset($_POST['order'])) 
+    //     {
+    //         $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+    //     } 
+    //     else if(isset($this->order))
+    //     {
+    //         $order = $this->order;
+    //         $this->db->order_by(key($order), $order[key($order)]);
+    //     }
+    // }
  
-    function get_datatables()
-    {
-        $this->_get_datatables_query();
-        if($_POST['length'] != -1)
-        $this->db->limit($_POST['length'], $_POST['start']);
-        $query = $this->db->get();
-        return $query->result();
-    }
+    // function get_datatables()
+    // {
+    //     $this->_get_datatables_query();
+    //     if($_POST['length'] != -1)
+    //     $this->db->limit($_POST['length'], $_POST['start']);
+    //     $query = $this->db->get();
+    //     return $query->result();
+    // }
  
-    function count_filtered()
-    {
-        $this->_get_datatables_query();
-        $query = $this->db->get();
-        return $query->num_rows();
-    }
+    // function count_filtered()
+    // {
+    //     $this->_get_datatables_query();
+    //     $query = $this->db->get();
+    //     return $query->num_rows();
+    // }
  
-    public function count_all()
-    {
-        $this->db->from($this->table);
-        return $this->db->count_all_results();
-    }
+    // public function count_all()
+    // {
+    //     $this->db->from($this->table);
+    //     return $this->db->count_all_results();
+    // }
 
-    function delete_users($kd_user)
+    // seq adalah kode user
+
+    function delete_users($seq)
     {        
-        $this->db->where('kd_user', $kd_user);
-        $result=$this->db->delete('users');
+        $this->db->where('seq', $seq);
+        $result=$this->db->delete('zuser');
         return $result;
     }
 
-    public function update_user($kd_user, $data1)
+    public function update_user($seq, $data1)
     {
         $this->db->set($data1);
-        $this->db->where('kd_user', $kd_user);
-        $this->db->update('users');
+        $this->db->where('seq', $seq);
+        $this->db->update('zuser');
     }
 
     public function tampil_permintaan_registrasi()
@@ -125,18 +127,18 @@ class m_users extends CI_Model {
         return $result->result();
     }
 
-    public function konfirmasi_register($kd_user, $isi)
+    public function konfirmasi_register($seq, $isi)
     {
         $this->db->set($isi);
-        $this->db->where('kd_user', $kd_user);
-        $this->db->update('users');
+        $this->db->where('seq', $seq);
+        $this->db->update('zuser');
     }
 
-    public function status_update($kd_user, $isi)
+    public function status_update($seq, $isi)
     {
         $this->db->set($isi);
-        $this->db->where('kd_user', $kd_user);
-        $this->db->update('users');
+        $this->db->where('seq', $seq);
+        $this->db->update('zuser');
     }
 
 
